0449d5b1c002fd31319717dd48f1bf5f38942108
Ezequielagradnik <ezequielagradnik@gmail.com>
2024-12-25 22:46:24 -0300

    terminado el header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,34 +5,66 @@ import '../Header.css';
 const Header = ({ user }) => {
   return (
     <nav className="header-container">
-      <div className="logo-section">
-        <img src="/catalog_images/logo_todo_lindo.png" alt="Logo Todo Lindo" className="header-logo" />
+    <div className="logo-section">
+      <Link to="/">
+        <img
+          src="/catalog_images/logo_todo_lindo.png"
+          alt="Logo Todo Lindo"
+          className="header-logo"
+        />
+      </Link>
       </div>
       <div className="nav-links">
         <div className="dropdown">
-          <span>Explorar por categoría</span>
+          <span>▼ Explorar por categoría</span>
           <ul>
-            <li><Link to="/category/bazar">Bazar</Link></li>
-            <li><Link to="/category/jugueteria">Juguetería</Link></li>
-            <li><Link to="/category/libreria">Librería</Link></li>
-            <li><Link to="/category/accesorios">Accesorios</Link></li>
+            <li>
+              <Link to="/category/bazar">Bazar</Link>
+            </li>
+            <li>
+              <Link to="/category/jugueteria">Juguetería</Link>
+            </li>
+            <li>
+              <Link to="/category/libreria">Librería</Link>
+            </li>
+            <li>
+              <Link to="/category/accesorios">Accesorios</Link>
+            </li>
           </ul>
         </div>
-        <Link to="/offers" className="offers-button">OFERTAS EXCLUSIVAS</Link>
+        <Link to="/offers" className="offers-button">
+          OFERTAS EXCLUSIVAS
+        </Link>
         <div className="search-bar">
           <input type="text" placeholder="Buscar..." />
+          <div className="search-icon-wrapper">
+          </div>
         </div>
       </div>
       <div className="icons-section">
-        <Link to="/cart">
-          <img src="/catalog_images/cart_icon.png" alt="Carrito" className="header-icon" />
-        </Link>
-        <Link to="/notifications">
-          <img src="/catalog_images/notification_icon.png" alt="Notificaciones" className="header-icon" />
-        </Link>
-        <Link to="/register">
-          <img src="/catalog_images/user_icon.png" alt="Usuario" className="header-icon" />
-        </Link>
+        <div className="header-icon-wrapper">
+          <img
+            src="/catalog_images/cart_icon.png"
+            alt="Carrito"
+            className="header-icon"
+          />
+        </div>
+        <div className="header-icon-wrapper">
+          <img
+            src="/catalog_images/notification_icon.png"
+            alt="Notificaciones"
+            className="header-icon"
+          />
+        </div>
+        <div className="header-icon-wrapper">
+          <Link to="/register">
+            <img
+              src="/catalog_images/user_icon.png"
+              alt="Usuario"
+              className="header-icon"
+            />
+          </Link>
+        </div>
       </div>
     </nav>
   );
