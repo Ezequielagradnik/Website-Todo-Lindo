9b2d892c199bb87209baf525ca4ff52798473aaa
Ezequielagradnik <ezequielagradnik@gmail.com>
2024-12-29 23:41:58 -0300

    feat gitignore in both sides & fix oferts

--- frontend/src/pages/Ofertas.js
+++ frontend/src/pages/Ofertas.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const offers = [
   { id: 1, name: 'Termo en Oferta', price: 800, description: 'Descuento especial del 20%.' },
-  { id: 2, name: 'Mate Oferta', price: 500, description: 'Precio rebajado por tiempo limitado.' },
+  { id: 2, name: 'Mate en Oferta', price: 500, description: 'Precio rebajado por tiempo limitado.' },
+  {id:3, name: 'Espadas 2x1', price:500, description:'¡2X1!'}
+
 ];
 
 const Offers = () => {
